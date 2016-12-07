3f9749ec8b4f1bd78d0e434044644aab3d4aa108
Csanad Beres <qed.csberes@gmail.com>
2016-12-07 12:13:47 +0100

    test: implement KeyValue tests

---
+++ src/key-value.spec.js
@@ -0,0 +1,23 @@
+describe('ReactKeyValue', () => {
+  it('should have the proper class name');
+  it('should set the default props');
+  it('should get the initial state from the props');
+  it('should handle adding new rows');
+  it('should handle when a key changes');
+  it('should handle when a value changes');
+  it('should handle removing a row');
+  it('should render a KeyItem');
+  it('should render a ValueItem');
+  it('should have a renderRows method');
+  it('should have a renderAddButton method');
+
+  describe('react-key-value-rows', () => {
+    it('should have the proper class name');
+    it('should call the renderRows method');
+  });
+
+  describe('react-key-value-add-new', () => {
+    it('should have the proper class name');
+    it('should call the renderAddButton method');
+  });
+});
