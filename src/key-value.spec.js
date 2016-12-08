f80e5aad8f0c4a35d07562d0f1416469e43718d2
Csanad Beres <qed.csberes@gmail.com>
2016-12-08 20:26:33 +0100

    test: implement testing of input field changes

--- src/key-value.spec.js
+++ src/key-value.spec.js
@@ -34,6 +34,28 @@ describe('ReactKeyValue', () => {
       { keyItem: 'z', valueItem: 'A' }
     ]);
   });
+  it('should update the state when a key changes', () => {
+    const $ = shallow(
+      <KeyValue
+        rows={ [{ keyItem: 'a', valueItem: 'A' }] }
+      />
+    );
+    $.find('.key-value-row-key-item').at(0).find('input').simulate('change', { currentTarget: { value: 'z' }} );
+    $.state('rows').should.eql([
+      { keyItem: 'z', valueItem: 'A' }
+    ]);
+  });
+  it('should update the state when a value changes', () => {
+    const $ = shallow(
+      <KeyValue
+        rows={ [{ keyItem: 'a', valueItem: 'A' }] }
+      />
+    );
+    $.find('.key-value-row-value-item').at(0).find('input').simulate('change', { currentTarget: { value: 'Z' }} );
+    $.state('rows').should.eql([
+      { keyItem: 'a', valueItem: 'Z' }
+    ]);
+  });
   it('should handle when a value changes', () => {
     const $ = shallow(<KeyValue rows={ [{ keyItem: 'a', valueItem: 'A' }] }/>);
     $.instance().handleValueItemChange(0, 'Z');
